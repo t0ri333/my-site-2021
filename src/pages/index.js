469f40209a8c6e81c26482ba0d3932827031ab75
Madstick <ziliangz@gmail.com>
2021-06-15 16:31:07 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () =>  {
   return (
     <Layout pageTitle="Home Page">
       <p>This is just some random introduction paragraph.</p>
+      <StaticImage 
+      alt="random image"
+      src="../images/ballon.jpeg"
+      />
     </Layout>
   )
 }
